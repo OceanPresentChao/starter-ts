2653cfbb1a24b5814af49610b173edbb79a603e3
OceanPresent <oceanpresent@163.com>
2022-05-06 00:08:56 +0800

    🔧 chore: package.json

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,6 +1,6 @@
 import { sum } from '../index';
 describe("test", () => {
     test('adds 1 + 2 to equal 3', () => {
-        expect(sum(1, 2)).toBe(3);
+        expect(sum(5, 52)).toMatchSnapshot(`test`)
     });
 })
